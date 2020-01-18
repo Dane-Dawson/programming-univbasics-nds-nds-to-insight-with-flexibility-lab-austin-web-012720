83ad26f6bdf487a8f6ab8fedf50074cc8835460f
Dane P Dawson <dane.p.dawson@gmail.com>
2020-01-18 04:35:08 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -134,7 +134,7 @@ def movies_with_directors_set(source)
   #
   # RETURN:
   #
-  # Array of Hashes containing all of a director's movies. Each movie will need
+  # Array of arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
   
 end
