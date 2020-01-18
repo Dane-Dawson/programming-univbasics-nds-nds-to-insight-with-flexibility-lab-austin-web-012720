0f82a24d78d4d58cdf50a4be2900da7f727d68e0
Dane P Dawson <dane.p.dawson@gmail.com>
2020-01-18 03:39:29 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -131,7 +131,7 @@ def movies_with_directors_set(source)
   end
   director_array = flatten_a_o_a(director_array)
   
-director_array
+  director_array
     
   # GOAL: For each director, find their :movies Array and stick it in a new Array
   #
